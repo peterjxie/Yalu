97cd9390f13a9fb095bed0b8dcb19c976562023a
peterjxie <peter.j.xie@gmail.com>
2026-09-01 18:34:46 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "echelon-v1.26";
+var CACHE_NAME = "echelon-v1.27";
 var ROOT = self.registration.scope;
 var INDEX_URL = new URL("./index.html", ROOT).toString();
 var MANIFEST_URL = new URL("./manifest.webmanifest", ROOT).toString();
